47bc7eda926429656759c660fc58787c73643ea4
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-06-12 00:21:09 +0700

    Menambahkan fitur add my project menggunakan js sehingga akan tampil card project di bawah sesuai dengan inputan pada form add my project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
+//. Contact Form
+
+const contactForm = []
+
 const getContactForm = () => {
 
-    let name = document.getElementById('input-name').value
-    let email = document.getElementById('input-email').value
-    let phone = document.getElementById('input-phone').value
-    let subject = document.getElementById('select-subject').value
-    let message = document.getElementById('textarea-message').value
+    let name = document.getElementById('contactName').value
+    let email = document.getElementById('contactEmail').value
+    let phone = document.getElementById('contactPhone').value
+    let subject = document.getElementById('contactSubject').value
+    let message = document.getElementById('contactMessage').value
 
     if(name == ''){
         return alert('Harap Masukkan Nama!')
@@ -20,12 +24,159 @@ const getContactForm = () => {
 
     const a = document.createElement("a");
     a.href = `mailto:acep.awaludin179@gmail.com?subject=${subject}&body=Halo, nama saya adalah ${name}. ${message}.Oleh karena itu, saya tertarik merekrut anda untuk menjadi seorang ${subject} di perusahaan kami. Silakan hubungi kami melalui ${email} atau ${phone}`;
+    a.target = '_blank'
     a.click();
 
-console.log(name)
-console.log(email)
-console.log(phone)
-console.log(subject)
-console.log(message)
+    let dataContactForm = {
+        name,
+        email,
+        phone,
+        subject,
+        message
+    }
 
+    contactForm.push(dataContactForm);
+
+console.log(contactForm)
+
+}
+
+//. My Project Form
+
+const projectForm = []
+
+const getProjectForm = () => {
+
+    //. Name & Description
+    let name = document.getElementById('projectName').value
+    let description = document.getElementById('projectDescription').value
+    
+    //. Duration
+    let startDate = document.getElementById('projectStartDate').value
+    let endDate = document.getElementById('projectEndDate').value
+
+    startMonth = Number(new Date(startDate).getMonth())
+    endMonth = Number(new Date(endDate).getMonth())
+    startYear = Number(new Date(startDate).getFullYear())
+    endYear = Number(new Date(endDate).getFullYear())
+
+    let yearDuration = endYear - startYear
+
+    let monthDuration = (endMonth+(12*yearDuration) - startMonth) % 12
+    
+    let showMonthDuration = ''
+    let showYearDuration = ''
+
+    if(monthDuration > 0){
+        showMonthDuration = `${monthDuration} bulan`
+    }
+        
+    if(yearDuration > 0){
+        showYearDuration = `${yearDuration} tahun`
+    }
+
+    // . Techologies
+    let technologies = []
+
+    let html = document.getElementById('projectHTML').checked
+    if (html === true){
+        html = document.getElementById('projectHTML').value
+        technologies.push(html)
+    }
+    
+    let css = document.getElementById('projectCSS').checked
+    if (css === true){
+        css = document.getElementById('projectCSS').value
+        technologies.push(css)
+    }
+
+    let js = document.getElementById('projectJS').checked
+    if (js === true){
+        js = document.getElementById('projectJS').value
+        technologies.push(js)
+    }
+    
+    let tailwind = document.getElementById('projectTailwind').checked
+    if (tailwind === true){
+        tailwind = document.getElementById('projectTailwind').value
+        technologies.push(tailwind)
+    }
+
+    let sass = document.getElementById('projectSASS').checked
+    if (sass === true){
+        sass = document.getElementById('projectSASS').value
+        technologies.push(sass)
+    }
+    
+    //. Image
+
+    let image = document.getElementById('projectImage').files
+
+    image = URL.createObjectURL(image[0])
+    
+    //. Set Project Form
+    let setProjectForm = {
+        name,
+        showMonthDuration,
+        showYearDuration,
+        description,
+        technologies,
+        image
+    }
+
+    projectForm.push(setProjectForm)
+
+    console.log(projectForm)
+    
+    renderProject()
+}
+
+const renderProject = () => {
+    document.getElementById('myProject').innerHTML = 
+    `
+        <h1>MY PROJECT</h1>
+        <div class="project-item">
+            <div class="project-img" style="background-image: URL('./../assets/img/project/gudangku.png');">
+            </div>
+            <a href="https://acep-awaludin179-project.on.drv.tw/gudangku/Index.html" target="_blank">
+                <h3>Gudangku -2022</h3>
+            </a>
+            <p class="project-time">durasi: 3 bulan</p>
+            <p>Gudangku adalah sebuah aplikasi manajemen gudang berbasis web.</p>
+            <div class="project-technologies">
+                <i class='bx bxl-html5'></i>
+                <i class='bx bxl-css3'></i>
+                <i class='bx bxl-javascript'></i>
+            </div>
+            <div class="project-button">
+                <p>edit</p>
+                <p>delete</p>
+            </div>
+        </div>
+    `
+
+    for(let i = 0; i < projectForm.length; i++){
+        document.getElementById('myProject').innerHTML +=
+        `
+        <div class="project-item">
+            <div class="project-img" style="background-image: URL('${projectForm[i].image}');">
+            </div>
+            <a href="${projectForm[i].image}" target="_blank">
+                <h3>${projectForm[i].name}</h3>
+            </a>
+            <p class="project-time">durasi: ${projectForm[i].showYearDuration} ${projectForm[i].showMonthDuration}</p>
+            <p>${projectForm[i].description}</p>
+            <div id="projectTechnologies${i}" class="project-technologies">
+            </div>
+            <div class="project-button">
+                <p>edit</p>
+                <p>delete</p>
+            </div>
+        </div>
+            
+            `
+            for(const a of projectForm[i].technologies){
+                document.getElementById(`projectTechnologies${i}`).innerHTML += a
+            }
+    }
 }
